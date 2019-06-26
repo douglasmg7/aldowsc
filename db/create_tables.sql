c9e75508a5db30e4460c7002918fb9e882a6f59d
douglasmg7 <douglasmg7@gmail.com>
2019-06-26 13:44:34 -0300

    new table product_history

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -4,27 +4,55 @@
 pragma foreign_keys = on;
 
 -- Products.
-create table product (
-    code                    text primary key,
-    brand                   text not null,
-    category                text not null,
-    description             text not null,
-    unit                    text not null,
-    multiple                integer not null,
-    dealer_price            real not null,
-    suggestion_price        real not null,
-    technical_description   text not null,
-    availability            boolean not null,
-    length                  integer not null, -- mm.
-    width                   integer not null, -- mm.
-    height                  integer not null, -- mm.
-    weight                  integer not null, -- gr.
-    picture_link            blob,
-    warranty_period         integer,  -- Days.
-    rma_procedure           text,
-    created_at              date not null,
-    changed_at              date not null,
-    changed                 boolean not null,
-    new                     boolean not null,
-    removed                 boolean not null
+CREATE TABLE product (
+    code                    TEXT NOT NULL,
+    brand                   TEXT NOT NULL,
+    category                TEXT NOT NULL,
+    description             TEXT NOT NULL,
+    unit                    TEXT NOT NULL,
+    multiple                INTEGER NOT NULL,
+    dealer_price            REAL NOT NULL,
+    suggestion_price        REAL NOT NULL,
+    technical_description   TEXT NOT NULL,
+    availability            BOOLEAN NOT NULL,
+    length                  INTEGER NOT NULL, -- mm.
+    width                   INTEGER NOT NULL, -- mm.
+    height                  INTEGER NOT NULL, -- mm.
+    weight                  INTEGER NOT NULL, -- gr.
+    picture_link            BLOB,
+    warranty_period         INTEGER,  -- Months.
+    rma_procedure           TEXT,
+    created_at              DATE NOT NULL,
+    changed_at              DATE NOT NULL,
+    changed                 BOOLEAN NOT NULL,
+    new                     BOOLEAN NOT NULL,
+    removed                 BOOLEAN NOT NULL,
+    PRIMARY KEY (code)
+);
+
+-- Products history.
+create table product_history (
+    code                    TEXT NOT NULL,
+    brand                   TEXT NOT NULL,
+    category                TEXT NOT NULL,
+    description             TEXT NOT NULL,
+    unit                    TEXT NOT NULL,
+    multiple                INTEGER NOT NULL,
+    dealer_price            REAL NOT NULL,
+    suggestion_price        REAL NOT NULL,
+    technical_description   TEXT NOT NULL,
+    availability            BOOLEAN NOT NULL,
+    length                  INTEGER NOT NULL, -- mm.
+    width                   INTEGER NOT NULL, -- mm.
+    height                  INTEGER NOT NULL, -- mm.
+    weight                  INTEGER NOT NULL, -- gr.
+    picture_link            BLOB,
+    warranty_period         INTEGER,  -- months.
+    rma_procedure           TEXT,
+    created_at              DATE NOT NULL,
+    changed_at              DATE NOT NULL,
+    changed                 BOOLEAN NOT NULL,
+    new                     BOOLEAN NOT NULL,
+    removed                 BOOLEAN NOT NULL,
+    PRIMARY KEY (code, created_at)
 );
